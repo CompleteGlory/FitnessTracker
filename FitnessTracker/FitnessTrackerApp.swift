262012d2f7332870a19fb98a955400ef30fd9bb0
CompleteGlory <magddevf@gmail.com>
2026-08-30 17:37:25 +0300

    Initial Commit

---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by Magd on 30/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessTrackerTests/FitnessTrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  FitnessTrackerTests.swift
+//  FitnessTrackerTests
+//
+//  Created by Magd on 30/08/2026.
+//
+
+import Testing
+@testable import FitnessTracker
+
+struct FitnessTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
